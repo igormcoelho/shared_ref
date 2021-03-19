53226aa6437472419471e7496b5582d004111987
Igor Machado <igor.machado@gmail.com>
2021-03-19 18:02:54 -0300

    more examples

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -5,14 +5,42 @@
 int
 main()
 {
+   // ==========================================
+   // easy to create by passing pointer
+   //
    nn::shared<int> p1(new int{ 9 });
-   //nn::shared<int> p2(nullptr); // OK, not allowed!
 
+   // ==========================================
+   // fails to create explicit nullptr (good thing!)
+   //
+   //nn::shared<int> p2(nullptr); // FAIL: not allowed!
+
+   // ==========================================
+   // copy shared ownership of object
+   //
    nn::shared<int> p3(p1);
+
+   // automatic conversion to internal type int
    std::cout << p3 << std::endl;
 
+   // automatic conversion to internal type int
+   auto p4 = p1 + p3;
+   std::cout << typeid(p4).name() << ": " << p4 << std::endl;
+   
+   // ==========================================
+   // cannot pass ownership of local reference (only pointers)
+   int x5 = 10;
+   //nn::shared<int> p5 = x5; // FAIL: cannot build from 'int'
+   nn::shared<int> p5 (new int{p1 + p4}); // OK: can pack pointer
+
+
+   // ==========================================
+   // works with complex types (such as strings)
+   //
    nn::shared<std::string> p_str(new std::string{ "hello world!" });
-   std::cout << (std::string) p_str << std::endl;
+   // note: for now, explicit cast is needed...
+   std::cout << (std::string)p_str << std::endl;
+   
 
    return 0;
 }
